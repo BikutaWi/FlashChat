c775046a844f589628c85c41ed3a1718006c7a41
William Bikuta <william.bikuta@he-arc.ch>
2022-06-24 23:14:33 +0200

    add cell XIB file

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,14 +7,23 @@ import UIKit
 import FirebaseAuth
 
 class ChatViewController: UIViewController {
-
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    
+    var messages: [Message] = [
+        Message(sender: "William@gmail.com", body: "That's the first message, just an example"),
+        Message(sender: "William@gmail.com", body: "You can send message here !"),
+        Message(sender: "William@gmail.com", body: "Have a great time !")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -28,5 +37,17 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
             print("Error signin out: %@", signOutError)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
     }
 }

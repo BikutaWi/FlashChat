782340cf4fcd1cf388b987e75fc3372dd6ee6197
William Bikuta <william.bikuta@he-arc.ch>
2022-06-24 22:34:00 +0200

    add login segue and logout button

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -12,6 +12,6 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = "⚡️FlashChat"
+        titleLabel.text = K.appName
     }
 }
